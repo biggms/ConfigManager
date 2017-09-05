534f6defb1f36c3261ff2d7b95a63b662bc6f41f
Graham Stewart <graham.stewart@tcsjohnhuxley.com>
2017-09-05 22:44:31 +0100

    Added configuration options for static ip

--- src/ConfigManager.cpp
+++ src/ConfigManager.cpp
@@ -8,316 +8,393 @@ const char mimeJSON[] PROGMEM = "application/json";
 const char mimePlain[] PROGMEM = "text/plain";
 
 Mode ConfigManager::getMode() {
-    return this->mode;
+	return this->mode;
 }
 
 void ConfigManager::setAPName(const char *name) {
-    this->apName = (char *)name;
+	this->apName = (char *)name;
 }
 
 void ConfigManager::setAPFilename(const char *filename) {
-    this->apFilename = (char *)filename;
+	this->apFilename = (char *)filename;
 }
 
 void ConfigManager::setAPTimeout(const int timeout) {
-    this->apTimeout = timeout;
+	this->apTimeout = timeout;
 }
 
 void ConfigManager::setWifiConnectRetries(const int retries) {
-    this->wifiConnectRetries = retries;
+	this->wifiConnectRetries = retries;
 }
 
 void ConfigManager::setWifiConnectInterval(const int interval) {
-    this->wifiConnectInterval = interval;
+	this->wifiConnectInterval = interval;
 }
 
 void ConfigManager::setAPCallback(std::function<void(ESP8266WebServer*)> callback) {
-    this->apCallback = callback;
+	this->apCallback = callback;
 }
 
 void ConfigManager::setAPICallback(std::function<void(ESP8266WebServer*)> callback) {
-    this->apiCallback = callback;
+	this->apiCallback = callback;
 }
 
 void ConfigManager::loop() {
-    if (mode == ap && apTimeout > 0 && ((millis() - apStart) / 1000) > apTimeout) {
-        ESP.restart();
-    }
+	if (mode == ap && apTimeout > 0 && ( ( millis() - apStart ) / 1000 ) > apTimeout) {
+		ESP.restart();
+	}
 
-    if (dnsServer) {
-        dnsServer->processNextRequest();
-    }
+	if (dnsServer) {
+		dnsServer->processNextRequest();
+	}
 
-    if (server) {
-        server->handleClient();
-    }
+	if (server) {
+		server->handleClient();
+	}
 }
 
 void ConfigManager::save() {
-    this->writeConfig();
+	this->writeConfig();
 }
 
 JsonObject &ConfigManager::decodeJson(String jsonString)
 {
-    DynamicJsonBuffer jsonBuffer;
+	DynamicJsonBuffer jsonBuffer;
 
-    if (jsonString.length() == 0) {
-        return jsonBuffer.createObject();
-    }
+	if (jsonString.length() == 0) {
+		return jsonBuffer.createObject();
+	}
 
-    JsonObject& obj = jsonBuffer.parseObject(jsonString);
+	JsonObject& obj = jsonBuffer.parseObject(jsonString);
 
-    if (!obj.success()) {
-        return jsonBuffer.createObject();
-    }
+	if ( !obj.success() ) {
+		return jsonBuffer.createObject();
+	}
 
-    return obj;
+	return obj;
 }
 
 void ConfigManager::handleAPGet() {
-    SPIFFS.begin();
+	SPIFFS.begin();
 
-    File f = SPIFFS.open(apFilename, "r");
-    if (!f) {
-        Serial.println(F("file open failed"));
-        server->send(404, FPSTR(mimeHTML), F("File not found"));
-        return;
-    }
+	File f = SPIFFS.open(apFilename, "r");
+	if (!f) {
+		Serial.println( F("file open failed") );
+		server->send( 404, FPSTR(mimeHTML), F("File not found") );
+		return;
+	}
 
-    server->streamFile(f, FPSTR(mimeHTML));
+	server->streamFile( f, FPSTR(mimeHTML) );
 
-    f.close();
+	f.close();
 }
 
 void ConfigManager::handleAPPost() {
-    bool isJson = server->header("Content-Type") == FPSTR(mimeJSON);
-    String ssid;
-    String password;
-    char ssidChar[32];
-    char passwordChar[64];
+	bool isJson = server->header("Content-Type") == FPSTR(mimeJSON);
+	String ssid;
+	String password;
+	String ipaddress;
+	String netmask;
+	String gateway;
+	String hostname;
 
-    if (isJson) {
-        JsonObject& obj = this->decodeJson(server->arg("plain"));
+	char ssidChar[32];
+	char passwordChar[64];
+	char ipaddressChar[15];
+	char netmaskChar[15];
+	char gatewayChar[15];
+	char hostnameChar[32];
 
-        ssid = obj.get<String>("ssid");
-        password = obj.get<String>("password");
-    } else {
-        ssid = server->arg("ssid");
-        password = server->arg("password");
-    }
+	if (isJson) {
+		JsonObject& obj = this->decodeJson( server->arg("plain") );
 
-    if (ssid.length() == 0) {
-        server->send(400, FPSTR(mimePlain), F("Invalid ssid or password."));
-        return;
-    }
+		ssid = obj.get<String>("ssid");
+		password = obj.get<String>("password");
+		ipaddress = obj.get<String>("ipaddress");
+		netmask = obj.get<String>("netmask");
+		gateway = obj.get<String>("gateway");
+		hostname = obj.get<String>("hostname");
+	} else {
+		ssid = server->arg("ssid");
+		password = server->arg("password");
+		ipaddress = server->arg("ipaddress");
+		netmask = server->arg("netmask");
+		gateway = server->arg("gateway");
+		hostname = server->arg("hostname");
+	}
 
-    strncpy(ssidChar, ssid.c_str(), sizeof(ssidChar));
-    strncpy(passwordChar, password.c_str(), sizeof(passwordChar));
+	if (ssid.length() == 0) {
+		server->send( 400, FPSTR(mimePlain), F("Invalid ssid or password.") );
+		return;
+	}
 
+	strncpy( ssidChar, ssid.c_str(), sizeof( ssidChar ) );
+	strncpy( passwordChar, password.c_str(), sizeof( passwordChar ) );
+	strncpy( ipaddressChar, ipaddress.c_str(), sizeof( ipaddressChar ) );
+	strncpy( netmaskChar, netmask.c_str(), sizeof( netmaskChar ) );
+	strncpy( gatewayChar, gateway.c_str(), sizeof( gatewayChar ) );
+	strncpy( hostnameChar, hostname.c_str(), sizeof( hostnameChar ) );
 
-    EEPROM.put(0, magicBytes);
-    EEPROM.put(WIFI_OFFSET, ssidChar);
-    EEPROM.put(WIFI_OFFSET + 32, passwordChar);
-    EEPROM.commit();
+	Serial.println( "Saving config: ");
+	Serial.print( "SSID:" );
+	Serial.print( ssid );
+	Serial.print( " Password:" );
+	Serial.print( password );
+	Serial.print( " Hostname:" );
+	Serial.print( hostname );
+	Serial.print( " IPAddress:" );
+	Serial.print( ipaddress );
+	Serial.print( " Netmask:" );
+	Serial.print( netmask );
+	Serial.print( " Gateway:" );
+	Serial.println( gateway );
 
-    server->send(204, FPSTR(mimePlain), F("Saved. Will attempt to reboot."));
+	EEPROM.put(0, magicBytes);
+	EEPROM.put(WIFI_OFFSET, ssidChar);
+	EEPROM.put(WIFI_OFFSET + 32, passwordChar);
+	EEPROM.put(WIFI_OFFSET + 32 + 64, ipaddressChar);
+	EEPROM.put(WIFI_OFFSET + 32 + 64 + 15, netmaskChar);
+	EEPROM.put(WIFI_OFFSET + 32 + 64 + 15 + 15, gatewayChar);
+	EEPROM.put(WIFI_OFFSET + 32 + 64 + 15 + 15 + 15, hostnameChar);
+	EEPROM.commit();
 
-    ESP.restart();
+	server->send( 204, FPSTR(mimePlain), F("Saved. Will attempt to reboot.") );
+
+	ESP.restart();
 }
 
 void ConfigManager::handleRESTGet() {
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& obj = jsonBuffer.createObject();
+	DynamicJsonBuffer jsonBuffer;
+	JsonObject& obj = jsonBuffer.createObject();
 
-    std::list<BaseParameter*>::iterator it;
-    for (it = parameters.begin(); it != parameters.end(); ++it) {
-        if ((*it)->getMode() == set) {
-            continue;
-        }
+	std::list<BaseParameter*>::iterator it;
+	for (it = parameters.begin(); it != parameters.end(); ++it) {
+		if ( ( *it )->getMode() == set ) {
+			continue;
+		}
 
-        (*it)->toJson(&obj);
-    }
+		( *it )->toJson(&obj);
+	}
 
-    String body;
-    obj.printTo(body);
+	String body;
+	obj.printTo(body);
 
-    server->send(200, FPSTR(mimeJSON), body);
+	server->send(200, FPSTR(mimeJSON), body);
 }
 
 void ConfigManager::handleRESTPut() {
-    JsonObject& obj = this->decodeJson(server->arg("plain"));
-    if (!obj.success()) {
-        server->send(400, FPSTR(mimeJSON), "");
-        return;
-    }
+	JsonObject& obj = this->decodeJson( server->arg("plain") );
+	if ( !obj.success() ) {
+		server->send(400, FPSTR(mimeJSON), "");
+		return;
+	}
 
-    std::list<BaseParameter*>::iterator it;
-    for (it = parameters.begin(); it != parameters.end(); ++it) {
-        if ((*it)->getMode() == get) {
-            continue;
-        }
+	std::list<BaseParameter*>::iterator it;
+	for (it = parameters.begin(); it != parameters.end(); ++it) {
+		if ( ( *it )->getMode() == get ) {
+			continue;
+		}
 
-        (*it)->fromJson(&obj);
-    }
+		( *it )->fromJson(&obj);
+	}
 
-    writeConfig();
+	writeConfig();
 
-    server->send(204, FPSTR(mimeJSON), "");
+	server->send(204, FPSTR(mimeJSON), "");
 }
 
 void ConfigManager::handleNotFound() {
-    if (!isIp(server->hostHeader()) ) {
-        server->sendHeader("Location", String("http://") + toStringIP(server->client().localIP()), true);
-        server->send(302, FPSTR(mimePlain), ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
-        server->client().stop();
-        return;
-    }
+	if ( !isIp( server->hostHeader() ) ) {
+		server->sendHeader("Location", String("http://") + toStringIP( server->client().localIP() ), true);
+		server->send(302, FPSTR(mimePlain), "");// Empty content inhibits Content-length header so we have to close the socket ourselves.
+		server->client().stop();
+		return;
+	}
 
-    server->send(404, FPSTR(mimePlain), "");
-    server->client().stop();
+	server->send(404, FPSTR(mimePlain), "");
+	server->client().stop();
 }
 
 bool ConfigManager::wifiConnected() {
-    Serial.print(F("Waiting for WiFi to connect"));
+	Serial.print( F("Waiting for WiFi to connect") );
 
-    int i = 0;
-    while (i < wifiConnectRetries) {
-        if (WiFi.status() == WL_CONNECTED) {
-            Serial.println("");
-            return true;
-        }
+	int i = 0;
+	while (i < wifiConnectRetries) {
+		if (WiFi.status() == WL_CONNECTED) {
+			Serial.println("");
+			return true;
+		}
 
-        Serial.print(".");
+		Serial.print(".");
 
-        delay(wifiConnectInterval);
-        i++;
-    }
+		delay(wifiConnectInterval);
+		i++;
+	}
 
-    Serial.println("");
-    Serial.println(F("Connection timed out"));
+	Serial.println("");
+	Serial.println( F("Connection timed out") );
 
-    return false;
+	return false;
 }
 
 void ConfigManager::setup() {
-    char magic[2];
-    char ssid[32];
-    char password[64];
+	char magic[2];
+	char ssid[32];
+	char password[64];
+	char ipaddressChar[15];
+	char netmaskChar[15];
+	char gatewayChar[15];
+	char hostname[32];
+	IPAddress ipaddress, netmask, gateway;
 
-    Serial.println(F("Reading saved configuration"));
+	Serial.println( F("Reading saved configuration:" ) );
 
-    EEPROM.get(0, magic);
-    EEPROM.get(WIFI_OFFSET, ssid);
-    EEPROM.get(WIFI_OFFSET + 32, password);
-    readConfig();
+	EEPROM.get(0, magic);
+	EEPROM.get(WIFI_OFFSET, ssid);
+	EEPROM.get(WIFI_OFFSET + 32, password);
+	EEPROM.get(WIFI_OFFSET + 32 + 64, ipaddressChar);
+	EEPROM.get(WIFI_OFFSET + 32 + 64 + 15, netmaskChar);
+	EEPROM.get(WIFI_OFFSET + 32 + 64 + 15 + 15, gatewayChar);
+	EEPROM.get(WIFI_OFFSET + 32 + 64 + 15 + 15 + 15, hostname);
 
-    if (memcmp(magic, magicBytes, 2) == 0) {
-        WiFi.begin(ssid, password[0] == '\0' ? NULL : password);
-        if (wifiConnected()) {
-            Serial.print(F("Connected to "));
-            Serial.print(ssid);
-            Serial.print(F(" with "));
-            Serial.println(WiFi.localIP());
+	Serial.print( "SSID:" );
+	Serial.print( ssid );
+	Serial.print( " Password:" );
+	Serial.print( password );
+	Serial.print( " Hostname:" );
+	Serial.print( hostname );
+	Serial.print( " IPAddress:" );
+	Serial.print( ipaddressChar );
+	Serial.print( " Netmask:" );
+	Serial.print( netmaskChar );
+	Serial.print( " Gateway:" );
+	Serial.println( gatewayChar );
 
-            WiFi.mode(WIFI_STA);
-            startApi();
-            return;
-        }
-    } else {
-        // We are at a cold start, don't bother timeing out.
-        apTimeout = 0;
-    }
 
-    startAP();
+	if (memcmp(magic, magicBytes, 2) == 0) {
+		if( hostname[0] != '\0' ) {
+			WiFi.hostname(hostname);
+		}
+		if( ipaddress.fromString( ipaddressChar) && netmask.fromString( netmaskChar ) && gateway.fromString( gatewayChar ) ) {
+			WiFi.config(ipaddress, gateway, netmask);
+		}
+		WiFi.mode(WIFI_STA);
+
+		WiFi.begin(ssid, password[0] == '\0' ? NULL : password);
+		if ( wifiConnected() ) {
+			Serial.print( F("Connected to ") );
+			Serial.print(ssid);
+			Serial.print( F(" with ") );
+			Serial.println( WiFi.localIP() );
+
+			readConfig();
+
+			startApi();
+			return;
+		}
+	} else {
+		// We are at a cold start, don't bother timeing out.
+		Serial.println( "New config, writing defaults" );
+		writeConfig();
+		apTimeout = 0;
+	}
+
+	startAP();
 }
 
 void ConfigManager::startAP() {
-    const char* headerKeys[] = {"Content-Type"};
-    size_t headerKeysSize = sizeof(headerKeys)/sizeof(char*);
+	const char* headerKeys[] = {"Content-Type"};
+	size_t headerKeysSize = sizeof( headerKeys ) / sizeof( char* );
 
-    mode = ap;
+	mode = ap;
 
-    Serial.println(F("Starting Access Point"));
+	Serial.println( F("Starting Access Point") );
 
-    IPAddress ip(192, 168, 1, 1);
-    WiFi.mode(WIFI_AP);
-    WiFi.softAPConfig(ip, ip, IPAddress(255, 255, 255, 0));
-    WiFi.softAP(apName);
+	IPAddress ip(192, 168, 1, 1);
+	WiFi.mode(WIFI_AP);
+	WiFi.softAPConfig( ip, ip, IPAddress(255, 255, 255, 0) );
+	WiFi.softAP(apName);
 
-    delay(500); // Need to wait to get IP
+	delay(500);	// Need to wait to get IP
 
-    dnsServer.reset(new DNSServer);
-    dnsServer->setErrorReplyCode(DNSReplyCode::NoError);
-    dnsServer->start(DNS_PORT, "*", ip);
+	dnsServer.reset(new DNSServer);
+	dnsServer->setErrorReplyCode(DNSReplyCode::NoError);
+	dnsServer->start(DNS_PORT, "*", ip);
 
-    server.reset(new ESP8266WebServer(80));
-    server->collectHeaders(headerKeys, headerKeysSize);
-    server->on("/", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleAPGet, this));
-    server->on("/", HTTPMethod::HTTP_POST, std::bind(&ConfigManager::handleAPPost, this));
-    server->onNotFound(std::bind(&ConfigManager::handleNotFound, this));
+	server.reset( new ESP8266WebServer(80) );
+	server->collectHeaders(headerKeys, headerKeysSize);
+	server->on( "/", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleAPGet, this) );
+	server->on( "/", HTTPMethod::HTTP_POST, std::bind(&ConfigManager::handleAPPost, this) );
+	server->onNotFound( std::bind(&ConfigManager::handleNotFound, this) );
 
-    if (apCallback) {
-        apCallback(server.get());
-    }
+	if (apCallback) {
+		apCallback( server.get() );
+	}
 
-    server->begin();
+	server->begin();
 
-    apStart = millis();
+	apStart = millis();
 }
 
 void ConfigManager::startApi() {
-    const char* headerKeys[] = {"Content-Type"};
-    size_t headerKeysSize = sizeof(headerKeys)/sizeof(char*);
+	configured = true;
+	const char* headerKeys[] = {"Content-Type"};
+	size_t headerKeysSize = sizeof( headerKeys ) / sizeof( char* );
 
-    mode = api;
+	mode = api;
 
-    server.reset(new ESP8266WebServer(80));
-    server->collectHeaders(headerKeys, headerKeysSize);
-    server->on("/", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleAPGet, this));
-    server->on("/", HTTPMethod::HTTP_POST, std::bind(&ConfigManager::handleAPPost, this));
-    server->on("/settings", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleRESTGet, this));
-    server->on("/settings", HTTPMethod::HTTP_PUT, std::bind(&ConfigManager::handleRESTPut, this));
-    server->onNotFound(std::bind(&ConfigManager::handleNotFound, this));
+	server.reset( new ESP8266WebServer(80) );
+	server->collectHeaders(headerKeys, headerKeysSize);
+	server->on( "/", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleAPGet, this) );
+	server->on( "/", HTTPMethod::HTTP_POST, std::bind(&ConfigManager::handleAPPost, this) );
+	server->on( "/settings", HTTPMethod::HTTP_GET, std::bind(&ConfigManager::handleRESTGet, this) );
+	server->on( "/settings", HTTPMethod::HTTP_PUT, std::bind(&ConfigManager::handleRESTPut, this) );
+	server->onNotFound( std::bind(&ConfigManager::handleNotFound, this) );
 
-    if (apiCallback) {
-        apiCallback(server.get());
-    }
+	if (apiCallback) {
+		apiCallback( server.get() );
+	}
 
-    server->begin();
+	server->begin();
+}
+
+bool ConfigManager::isConfigured()
+{
+	return configured;
 }
 
 void ConfigManager::readConfig() {
-    byte *ptr = (byte *)config;
+	byte *ptr = (byte *)config;
 
-    for (int i = 0; i < configSize; i++) {
-        *(ptr++) = EEPROM.read(CONFIG_OFFSET + i);
-    }
+	for (int i = 0; i < configSize; i++) {
+		*( ptr++ ) = EEPROM.read(CONFIG_OFFSET + i);
+	}
 }
 
 void ConfigManager::writeConfig() {
-    byte *ptr = (byte *)config;
+	byte *ptr = (byte *)config;
 
-    for (int i = 0; i < configSize; i++) {
-        EEPROM.write(CONFIG_OFFSET + i, *(ptr++));
-    }
-    EEPROM.commit();
+	for (int i = 0; i < configSize; i++) {
+		EEPROM.write( CONFIG_OFFSET + i, *( ptr++ ) );
+	}
+	EEPROM.commit();
 }
 
 boolean ConfigManager::isIp(String str) {
-  for (int i = 0; i < str.length(); i++) {
-    int c = str.charAt(i);
-    if (c != '.' && (c < '0' || c > '9')) {
-      return false;
-    }
-  }
-  return true;
+	for (int i = 0; i < str.length(); i++) {
+		int c = str.charAt(i);
+		if ( c != '.' && ( c < '0' || c > '9' ) ) {
+			return false;
+		}
+	}
+	return true;
 }
 
 String ConfigManager::toStringIP(IPAddress ip) {
-  String res = "";
-  for (int i = 0; i < 3; i++) {
-    res += String((ip >> (8 * i)) & 0xFF) + ".";
-  }
-  res += String(((ip >> 8 * 3)) & 0xFF);
-  return res;
+	String res = "";
+	for (int i = 0; i < 3; i++) {
+		res += String( ( ip >> ( 8 * i ) ) & 0xFF ) + ".";
+	}
+	res += String( ( ( ip >> 8 * 3 ) ) & 0xFF );
+	return res;
 }
--- src/ConfigManager.h
+++ src/ConfigManager.h
@@ -11,7 +11,7 @@
 #include "ArduinoJson.h"
 
 #define WIFI_OFFSET 2
-#define CONFIG_OFFSET 98
+#define CONFIG_OFFSET 175
 
 enum Mode {ap, api};
 
@@ -21,10 +21,10 @@ enum ParameterMode { get, set, both};
  * Base Parameter
  */
 class BaseParameter {
-public:
-    virtual ParameterMode getMode() = 0;
-    virtual void fromJson(JsonObject *json) = 0;
-    virtual void toJson(JsonObject *json) = 0;
+	public:
+		virtual ParameterMode getMode() = 0;
+		virtual void fromJson(JsonObject *json) = 0;
+		virtual void toJson(JsonObject *json) = 0;
 };
 
 /**
@@ -32,155 +32,158 @@ public:
  */
 template<typename T>
 class ConfigParameter : public BaseParameter {
-public:
-    ConfigParameter(const char *name, T *ptr, ParameterMode mode = both, std::function<void(const char*)> cb = NULL) {
-        this->name = name;
-        this->ptr = ptr;
-        this->cb = cb;
-        this->mode = mode;
-    }
+	public:
+		ConfigParameter(const char *name, T *ptr, ParameterMode mode = both, std::function<void(const char*)> cb = NULL) {
+			this->name = name;
+			this->ptr = ptr;
+			this->cb = cb;
+			this->mode = mode;
+		}
 
-    ParameterMode getMode() {
-        return this->mode;
-    }
+		ParameterMode getMode() {
+			return this->mode;
+		}
 
-    void fromJson(JsonObject *json) {
-        if (json->containsKey(name) && json->is<T>(name)) {
-            *ptr = json->get<T>(name);
-        }
-    }
+		void fromJson(JsonObject *json) {
+			if ( json->containsKey(name) && json->is<T>(name) ) {
+				*ptr = json->get<T>(name);
+			}
+		}
 
-    void toJson(JsonObject *json) {
-        json->set(name, *ptr);
+		void toJson(JsonObject *json) {
+			json->set(name, *ptr);
 
-        if (cb) {
-            cb(name);
-        }
-    }
+			if (cb) {
+				cb(name);
+			}
+		}
 
-private:
-    const char *name;
-    T *ptr;
-    std::function<void(const char*)> cb;
-    ParameterMode mode;
+	private:
+		const char *name;
+		T *ptr;
+		std::function<void(const char*)> cb;
+		ParameterMode mode;
 };
 
 /**
  * Config String Parameter
  */
 class ConfigStringParameter : public BaseParameter {
-public:
-    ConfigStringParameter(const char *name, char *ptr, size_t length, ParameterMode mode = both) {
-        this->name = name;
-        this->ptr = ptr;
-        this->length = length;
-        this->mode = mode;
-    }
+	public:
+		ConfigStringParameter(const char *name, char *ptr, size_t length, ParameterMode mode = both) {
+			this->name = name;
+			this->ptr = ptr;
+			this->length = length;
+			this->mode = mode;
+		}
 
-    ParameterMode getMode() {
-        return this->mode;
-    }
+		ParameterMode getMode() {
+			return this->mode;
+		}
 
-    void fromJson(JsonObject *json) {
-        if (json->containsKey(name) && json->is<char *>(name)) {
-            const char * value = json->get<const char *>(name);
+		void fromJson(JsonObject *json) {
+			if ( json->containsKey(name) && json->is<char *>(name) ) {
+				const char * value = json->get<const char *>(name);
 
-            memset(ptr,'\n',length);
-            strncpy(ptr, const_cast<char*>(value), length - 1);
-        }
-    }
+				memset(ptr,'\n',length);
+				strncpy(ptr, const_cast<char*>( value ), length - 1);
+			}
+		}
 
-    void toJson(JsonObject *json) {
-        json->set(name, ptr);
-    }
+		void toJson(JsonObject *json) {
+			json->set(name, ptr);
+		}
 
-private:
-    const char *name;
-    char *ptr;
-    size_t length;
-    ParameterMode mode;
+	private:
+		const char *name;
+		char *ptr;
+		size_t length;
+		ParameterMode mode;
 };
 
 /**
  * Config Manager
  */
 class ConfigManager {
-public:
-    ConfigManager() {}
+	public:
+		ConfigManager() {
+		}
 
-    Mode getMode();
-    void setAPName(const char *name);
-    void setAPFilename(const char *filename);
-    void setAPTimeout(const int timeout);
-    void setWifiConnectRetries(const int retries);
-    void setWifiConnectInterval(const int interval);
-    void setAPCallback(std::function<void(ESP8266WebServer*)> callback);
-    void setAPICallback(std::function<void(ESP8266WebServer*)> callback);
-    void loop();
+		Mode getMode();
+		void setAPName(const char *name);
+		void setAPFilename(const char *filename);
+		void setAPTimeout(const int timeout);
+		void setWifiConnectRetries(const int retries);
+		void setWifiConnectInterval(const int interval);
+		void setAPCallback(std::function<void(ESP8266WebServer*)> callback);
+		void setAPICallback(std::function<void(ESP8266WebServer*)> callback);
+		void loop();
 
-    template<typename T>
-    void begin(T &config) {
-        this->config = &config;
-        this->configSize = sizeof(T);
+		template<typename T>
+		void begin(T &config) {
+			this->config = &config;
+			this->configSize = sizeof( T );
 
-        EEPROM.begin(CONFIG_OFFSET + this->configSize);
+			EEPROM.begin(CONFIG_OFFSET + this->configSize);
 
-        setup();
-    }
+			setup();
+		}
 
-    template<typename T>
-    void addParameter(const char *name, T *variable) {
-        parameters.push_back(new ConfigParameter<T>(name, variable));
-    }
-    template<typename T>
-    void addParameter(const char *name, T *variable, ParameterMode mode) {
-        parameters.push_back(new ConfigParameter<T>(name, variable, mode));
-    }
-    void addParameter(const char *name, char *variable, size_t size) {
-        parameters.push_back(new ConfigStringParameter(name, variable, size));
-    }
-    void addParameter(const char *name, char *variable, size_t size, ParameterMode mode) {
-        parameters.push_back(new ConfigStringParameter(name, variable, size, mode));
-    }
-    void save();
+		template<typename T>
+		void addParameter(const char *name, T *variable) {
+			parameters.push_back( new ConfigParameter<T>(name, variable) );
+		}
+		template<typename T>
+		void addParameter(const char *name, T *variable, ParameterMode mode) {
+			parameters.push_back( new ConfigParameter<T>(name, variable, mode) );
+		}
+		void addParameter(const char *name, char *variable, size_t size) {
+			parameters.push_back( new ConfigStringParameter(name, variable, size) );
+		}
+		void addParameter(const char *name, char *variable, size_t size, ParameterMode mode) {
+			parameters.push_back( new ConfigStringParameter(name, variable, size, mode) );
+		}
+		void save();
+		bool isConfigured();
 
-private:
-    Mode mode;
-    void *config;
-    size_t configSize;
+	private:
+		Mode mode;
+		void *config;
+		size_t configSize;
+		bool configured = false;
 
-    char *apName = (char *)"Thing";
-    char *apFilename = (char *)"/index.html";
-    int apTimeout = 0;
-    unsigned long apStart = 0;
+		char *apName = (char *)"Thing";
+		char *apFilename = (char *)"/index.html";
+		int apTimeout = 0;
+		unsigned long apStart = 0;
 
-    int wifiConnectRetries = 20;
-    int wifiConnectInterval = 500;
+		int wifiConnectRetries = 20;
+		int wifiConnectInterval = 500;
 
-    std::unique_ptr<DNSServer> dnsServer;
-    std::unique_ptr<ESP8266WebServer> server;
-    std::list<BaseParameter*> parameters;
+		std::unique_ptr<DNSServer> dnsServer;
+		std::unique_ptr<ESP8266WebServer> server;
+		std::list<BaseParameter*> parameters;
 
-    std::function<void(ESP8266WebServer*)> apCallback;
-    std::function<void(ESP8266WebServer*)> apiCallback;
+		std::function<void(ESP8266WebServer*)> apCallback;
+		std::function<void(ESP8266WebServer*)> apiCallback;
 
-    JsonObject &decodeJson(String jsonString);
+		JsonObject &decodeJson(String jsonString);
 
-    void handleAPGet();
-    void handleAPPost();
-    void handleRESTGet();
-    void handleRESTPut();
-    void handleNotFound();
+		void handleAPGet();
+		void handleAPPost();
+		void handleRESTGet();
+		void handleRESTPut();
+		void handleNotFound();
 
-    bool wifiConnected();
-    void setup();
-    void startAP();
-    void startApi();
+		bool wifiConnected();
+		void setup();
+		void startAP();
+		void startApi();
 
-    void readConfig();
-    void writeConfig();
-    boolean isIp(String str);
-    String toStringIP(IPAddress ip);
+		void readConfig();
+		void writeConfig();
+		boolean isIp(String str);
+		String toStringIP(IPAddress ip);
 };
 
-#endif /* __CONFIGMANAGER_H__ */
+#endif	/* __CONFIGMANAGER_H__ */
